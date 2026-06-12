0d2cb1c6ea6227d64ace10f98604cd83d9eff849
Shemuel <156734018+Shemuel-bot@users.noreply.github.com>
2026-06-12 01:26:29 -0500

    Add Sidebar component and associated styles

---
+++ Frontend/src/Components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import   '../css/sideBar.css'
+
+export default function Sidebar(){
+    return (
+        <>
+            <div className='container'>
+                <h1>User</h1>
+                <button>Play</button>
+                <button>Academy</button>
+                <button>Training</button>
+                <button>Profile</button>
+                <button>Settings</button>
+            </div>
+        </>
+    )
+}
