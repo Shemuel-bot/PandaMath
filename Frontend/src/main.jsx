a114dfb584b631ca9b4d03538beddfa8470b99c9
Shemuel <156734018+Shemuel-bot@users.noreply.github.com>
2026-06-14 18:02:36 -0500

    Add react-router-dom for routing and refactor App component to use Outlet

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot,  } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import MainScreenCenter from './Components/MainScreenCenter.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    children: [
+      {
+        path: 'home',
+        element: <MainScreenCenter/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
