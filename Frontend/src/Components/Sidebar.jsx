c0c817579ce41dfec4945f6712380e670d705fe4
Shemuel <156734018+Shemuel-bot@users.noreply.github.com>
2026-06-14 18:07:21 -0500

    Add icons to Sidebar buttons and enhance button styles

--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -1,16 +1,34 @@
 import style from  '../css/sideBar.module.css'
+import playIcon from '../assets/puzzle.png'
+import academyIcon from '../assets/education.png'
+import trainingIcon from '../assets/podium.png'
+import profileIcon from '../assets/user.png'
+import settingsIcon from '../assets/setting.png'
 
 export default function Sidebar(){
     return (
-        <>
-            <div className={style.container}>
-                <h1>User</h1>
-                <button>Play</button>
-                <button>Academy</button>
-                <button>Training</button>
-                <button>Profile</button>
-                <button>Settings</button>
-            </div>
-        </>
+        <div className={style.container}>
+            <h1>User</h1>
+            <button>
+                <img src={playIcon} alt="Play icon" className={style.icon} />
+                Play
+            </button>
+            <button>
+                <img src={academyIcon} alt="Academy icon" className={style.icon} />
+                Academy
+            </button>
+            <button>
+                <img src={trainingIcon} alt="Training icon" className={style.icon} />
+                Training
+            </button>
+            <button>
+                <img src={profileIcon} alt="Profile icon" className={style.icon} />
+                Profile
+            </button>
+            <button>
+                <img src={settingsIcon} alt="Settings icon" className={style.icon} />
+                Settings
+            </button>
+        </div>
     )
 }
